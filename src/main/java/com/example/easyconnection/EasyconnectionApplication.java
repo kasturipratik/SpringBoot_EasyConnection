b39e915c5fbfc799347797a7b8a1360a5c05bf0e
kasturipratik <pratik.parajuli@yahoo.com>
2018-06-29 15:15:20 -0400

    home page created

---
+++ src/main/java/com/example/easyconnection/EasyconnectionApplication.java
@@ -0,0 +1,12 @@
+package com.example.easyconnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyconnectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyconnectionApplication.class, args);
+	}
+}
