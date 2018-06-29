12eb0c0b086a2ee66211e1f42607ce7959b282f0
kasturipratik <pratik.parajuli@yahoo.com>
2018-06-29 16:58:35 -0400

    added employerSignup and still workin on it

---
+++ src/main/java/com/example/easyconnection/HomeController.java
@@ -0,0 +1,19 @@
+package com.example.easyconnection;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/createEmployer")
+    public String createEmployer(){
+        return "employerSignUp";
+    }
+
+}
